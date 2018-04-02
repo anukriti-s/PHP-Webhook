508c63a54bf5a2262adaac121167cea218693d0f
Srivastava <Anukriti.Srivastava@Honeywell.com>
2018-04-02 15:42:24 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$method=$_SERVER['REQUEST_METHOD'];
+
+//IN CASE OF POST METHOD
+if($method=="POST"){
+  $requestBody=file_get_contents('php://input');
+  $json=json_decode($request_body);
+  $text=$json->result->parameters->text;
+  switch($text){
+    case 'hi':
+      $speech="Hi, Nice to meet you.";
+      break;
+    case 'bye':
+      $speech="Bye, good night";
+      break;
+    case 'anything':
+      $speech="Yes, you can type anything here.";
+      break;
+    default:
+      $speech="Sorry, I didn't get that. Please ask me something else.";
+      break;
+  }
+  $response=new \stdClass();
+  $response->speech="";
+  $response->displayText="";
+  $response->source="Webhook";
+  echo json_encode($response);
+}
+else
+{ echo "Method not allowed";
+}
+?>
